c0b315bcfdb3f1863b1c0ad91460486cb57a1001
shooter <shooter.hero@gmail.com>
2015-02-05 20:04:18 +0800

    Initial Commit

---
+++ ruby-china_iosTests/ruby_china_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  ruby_china_iosTests.swift
+//  ruby-china_iosTests
+//
+//  Created by shooter on 2/5/15.
+//  Copyright (c) 2015 shooter. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ruby_china_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
