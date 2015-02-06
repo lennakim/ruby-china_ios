d436c36a76cb0d0652be1e6bf44452e814fc4221
shooter <shooter.hero@gmail.com>
2015-02-06 16:18:04 +0800

    更新 接口文件

--- ruby-china_ios/api.swift
+++ ruby-china_ios/api.swift
@@ -10,3 +10,13 @@ import Foundation
 
 let Domain = "https://ruby-china.org/api"
 let Topics = Domain + "/topics.json"
+let Users = Domain + "/users.json"
+let Nodes = Domain + "/nodes.json"
+
+func UserOfTopics(id: String) -> String {
+  return Domain + "/users/\(id)/topics.json"
+}
+
+func Topic(id: String) -> String {
+  return  Domain + "/topics/\(id).json"
+}
