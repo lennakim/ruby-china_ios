ad439cefc093e14c04feeda136356d10e39f3919
shooter <shooter.hero@gmail.com>
2015-02-10 15:56:49 +0800

    add  view controller

---
+++ ruby-china_ios/controllers/TopicDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  TopicDetailController.swift
+//  ruby-china_ios
+//
+//  Created by shooter on 2/10/15.
+//  Copyright (c) 2015 shooter. All rights reserved.
+//
+
+import UIKit
+
+class TopicDetailController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
